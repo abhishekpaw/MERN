eda0e16a77a13bea7d63bddad8aed36547dc1cbc
Abhishek Pawar <apawar0078@gmail.com>
2025-05-26 23:28:23 +0530

    Dashboard Statistics API

--- shopping-site-backend/dist/app.js
+++ shopping-site-backend/dist/app.js
@@ -9,6 +9,7 @@ import userRoute from "./routes/user.js";
 import productRoute from "./routes/product.js";
 import orderRoute from "./routes/order.js";
 import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 config({
     path: "./.env"
 });
@@ -26,6 +27,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
--- shopping-site-backend/src/app.ts
+++ shopping-site-backend/src/app.ts
@@ -11,6 +11,7 @@ import userRoute from "./routes/user.js";
 import productRoute from "./routes/product.js";
 import orderRoute from "./routes/order.js";
 import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 
 
 
@@ -40,6 +41,8 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/product",productRoute);
 app.use("/api/v1/order",orderRoute);
 app.use("/api/v1/payment",paymentRoute);
+app.use("/api/v1/dashboard",dashboardRoute);
+
 app.use(morgan("dev"));
 
 
