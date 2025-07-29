62ba290deaa7127c41b10145d510d216120d0088
Abhishek Pawar <apawar0078@gmail.com>
2025-07-29 18:00:09 +0530

    push small issue

--- shopping-site-frontend/src/App.tsx
+++ shopping-site-frontend/src/App.tsx
@@ -14,7 +14,7 @@ import ProtectedRoute from './components/protected-route';
 const Home = lazy(() => import('./Pages/home'));
 const Seacrh = lazy(() => import('./Pages/search'));
 const ProductDetails = lazy(() => import('./Pages/product-details'));
-const Cart = lazy(() => import('./Pages/Cart'));
+const Cart = lazy(() => import('./Pages/cart'));
 const Shipping = lazy(() => import('./Pages/Shipping'));
 const Login = lazy(() => import('./Pages/Login'));
 const Orders = lazy(() => import('./Pages/Orders'));
