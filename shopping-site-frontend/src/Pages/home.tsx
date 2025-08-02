94f11b7b17d4f2fdcf20892a77b595bf3844b62c
Abhishek Pawar <apawar0078@gmail.com>
2025-08-02 20:13:24 +0530

    new home section added

--- shopping-site-frontend/src/Pages/home.tsx
+++ shopping-site-frontend/src/Pages/home.tsx
@@ -6,10 +6,14 @@ import ProductCard from "../components/product-card";
 import { useLatestProductsQuery } from "../redux/api/productAPI";
 import { addToCart } from "../redux/reducer/cartReducer";
 import type { CartItem } from "../types/types";
+import { useEffect, useRef, useState } from "react";
+import "../styles/home.scss";
 
 const Home = () => {
 
   const {data,isLoading,isError} = useLatestProductsQuery("");
+  const sectionRef = useRef<HTMLDivElement | null>(null)
+  const [isVisible, setIsVisible] = useState(false)
 
   const dispatch = useDispatch();
 
@@ -21,9 +25,33 @@ const Home = () => {
 
   if (isError) toast.error("Cannot Fetch the Products.");
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+          observer.disconnect()
+        }
+      },
+      { threshold: 0.2 }
+    )
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current)
+    }
+
+    return () => observer.disconnect()
+  }, [])
+
   return (
     <div className="home">
-      <section></section>
+      <section ref={sectionRef}
+      className={`hero-section ${isVisible ? "show" : ""}`}>
+        <div className="hero-content">
+          <h1>Welcome to MERN Ecommerce</h1>
+          <p>Discover amazing products at the best prices.</p>
+        </div>
+      </section>
 
       <h1>
         Latest Products
